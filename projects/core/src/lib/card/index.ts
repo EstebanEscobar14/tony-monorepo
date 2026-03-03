27518199a9a8071f0d9c96179f1d5e433e90f273
juan.escobar <juan.escobar@nter.es>
2026-03-03 17:00:00 +0100

    feat: add card, input and textarea building blocks

---
+++ projects/core/src/lib/card/index.ts
@@ -0,0 +1,9 @@
+export {
+  TonCardDirective,
+  TonCardHeaderDirective,
+  TonCardTitleDirective,
+  TonCardDescriptionDirective,
+  TonCardContentDirective,
+  TonCardFooterDirective,
+} from './card.directives';
+export { cardVariants, type CardVariant, type CardPadding } from './card.variants';
---
+++ projects/core/src/lib/input/index.ts
@@ -0,0 +1,3 @@
+export { TonInputDirective } from './input.directive';
+export { TonLabelDirective } from './label.directive';
+export { inputVariants, labelVariants, type InputVariant, type InputSize } from './input.variants';
